89f625918002919bdbd3667ecbb79ae0e12c4f33
niissshan <nikitashanbhag9@gmail.com>
2025-12-16 07:58:51 +0530

    Fix BASE_URL

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import io from "socket.io-client";
 import "./Dashboard.css";
-const BASE_URL = "https://stock-broker-app.onrender.com/";
+const BASE_URL = "https://stock-broker-app.onrender.com";
+
 
 
 /* LOGOS */
